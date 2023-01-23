97bb842bee4febe52e6ec139673207e3a2566520
Daniel Tull <dt@danieltull.co.uk>
2023-01-23 10:29:11 +0000

    Initial Commit

---
+++ ScrollingViewThatFits/ScrollingViewThatFitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingViewThatFitsApp.swift
+//  ScrollingViewThatFits
+//
+//  Created by Daniel Tull on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingViewThatFitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
