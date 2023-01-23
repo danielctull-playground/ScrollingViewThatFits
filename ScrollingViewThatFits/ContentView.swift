d806d2e406ddb4ffd99f52eb6f7572160f9535a7
Daniel Tull <dt@danieltull.co.uk>
2023-01-23 10:34:03 +0000

    Add some long content

--- ScrollingViewThatFits/ContentView.swift
+++ ScrollingViewThatFits/ContentView.swift
@@ -1,19 +1,26 @@
-//
-//  ContentView.swift
-//  ScrollingViewThatFits
-//
-//  Created by Daniel Tull on 23/01/2023.
-//
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ViewThatFits(in: .vertical) {
+            LongContent()
+            ScrollView {
+                LongContent()
+            }
+        }
+    }
+}
+
+struct LongContent: View {
+
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+            Button {
+            } label: {
+                Text("Hello")
+            }
         }
         .padding()
     }
